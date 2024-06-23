ff17e73b5ebf4aa43b9b09375f13eb306ee0acf0
Sunan <sunangao@163.com>
2024-06-22 20:34:20 -0400

    final modification

--- 02_src/defined_functions.R
+++ 02_src/defined_functions.R
@@ -22,3 +22,12 @@ ActualvsPredict <- function(data.test,
     lty = c('solid','dashed')
   )
 }
+
+
+## Calculate Rsquare
+Rsquare <- function(predicted_values, actual_values){
+  SS_total <- sum((actual_values - mean(actual_values))^2)
+  SS_residual <- sum((actual_values - predicted_values)^2)
+  R2 <- 1 - (SS_residual / SS_total)
+  return(R2)
+}
